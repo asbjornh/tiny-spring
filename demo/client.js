39343eaa8ec1da37876ec93e0e5b24db74b7a33a
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-10-30 21:06:47 +0100

    Adds mouse-controlled spring demo

--- demo/client.js
+++ demo/client.js
@@ -1,4 +1,4 @@
-import Spring from '../spring';
+import Spring from '../spring.ts';
 
 const box = document.getElementById('box');
 const button = document.getElementById('button');
@@ -13,4 +13,15 @@ let value = 0;
 button.addEventListener('click', () => {
   value = value === 0 ? 300 : 0;
   spring.transitionTo(value);
+});
+
+const box2 = document.getElementById('box2');
+const spring2 = Spring(0, { stiffness: 50 });
+
+spring2.onUpdate(val => {
+  box2.style.transform = `translateX(${val}px)`;
+});
+
+window.addEventListener('mousemove', e => {
+  spring2.transitionTo(e.clientX);
 });
